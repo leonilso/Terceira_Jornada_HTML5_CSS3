b2553fbffcc9770dfdaf298cb639426b71df0cfa
Leonilso <129322347+leonilso@users.noreply.github.com>
2023-09-20 15:42:04 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,20 +1,26 @@
 const botao = document.getElementById("botao")
 const teste = document.querySelector(".teste")
+const direita = document.querySelector(".direita")
+const esquerda = document.querySelector(".esquerda")
 
 
 
-botao.addEventListener("click", () => {
-    teste.style.animation = "none"
-    setTimeout(() => 
-    teste.style.animation = "example 1s linear", 5);
-});
+// botao.addEventListener("click", () => {
+//     teste.style.animation = "none"
+//     setTimeout(() => 
+//     teste.style.animation = "example 1s linear", 5);
+// });
 
 teste.addEventListener("mouseover", () =>{
     teste.style.animation = "none"
     setTimeout(() => 
-    teste.style.animation = "example 1s infinite", 5);
+    teste.style.animation = "example 1s infinite", 250);
+    direita.style.animation = "example 1s infinite";
+    esquerda.style.animation = "example 1s infinite";
 });
 
 teste.addEventListener("mouseout", () =>{
     teste.style.animation = "none"
+    direita.style.animation = "none";
+    esquerda.style.animation = "none";
 });
