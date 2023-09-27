f6d62418700433a0079ca7651f58446b07949dff
Leonilso <129322347+leonilso@users.noreply.github.com>
2023-09-27 14:32:24 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,26 +1,19 @@
 const botao = document.getElementById("botao")
-const teste = document.querySelector(".teste")
-const direita = document.querySelector(".direita")
-const esquerda = document.querySelector(".esquerda")
+const heartdown = document.querySelector(".heartdown")
+const heartleft = document.querySelector(".heartleft")
+const heartright = document.querySelector(".heartright")
 
-
-
-// botao.addEventListener("click", () => {
-//     teste.style.animation = "none"
-//     setTimeout(() => 
-//     teste.style.animation = "example 1s linear", 5);
-// });
-
-teste.addEventListener("mouseover", () =>{
-    teste.style.animation = "none"
-    setTimeout(() => 
-    teste.style.animation = "example 1s infinite", 250);
-    direita.style.animation = "example 1s infinite";
-    esquerda.style.animation = "example 1s infinite";
+heartdown.addEventListener("mouseover", () =>{
+    heartdown.style.animation = "none"
+    heartleft.style.animation = "none"
+    heartright.style.animation = "none"
+    heartdown.style.animation = "example 1s infinite"
+    heartleft.style.animation = "example 1s infinite";
+    heartright.style.animation = "example 1s infinite";
 });
 
-teste.addEventListener("mouseout", () =>{
-    teste.style.animation = "none"
-    direita.style.animation = "none";
-    esquerda.style.animation = "none";
+heartdown.addEventListener("mouseout", () =>{
+    heartdown.style.animation = "none"
+    heartleft.style.animation = "none";
+    heartright.style.animation = "none";
 });
