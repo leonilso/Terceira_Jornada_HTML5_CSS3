f567853c19522809438f71dd3efb61e3450dd0f7
Leonilso <129322347+leonilso@users.noreply.github.com>
2023-10-25 14:59:50 -0300

    tenso

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const imagem = document.querySelector('#game-img')
 const position = [3, -3];
 
-imagem.addEventListener('mouseeneter', () =>{
+imagem.addEventListener('mouseenter', () =>{
     imagem.style.transform = "rotate(", Math.random(position), "deg) scale(1.3)";
+
 });
