28ad7d0b550e0e8dbc5feb0a028163652a79213c
Leonilso <129322347+leonilso@users.noreply.github.com>
2023-10-25 15:16:06 -0300

    random

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const imagem = document.querySelector('#game-img')
 
 
 imagem.addEventListener('mouseenter', () =>{
-    let rotacao = Math.random()*3
+    let rotacao = Math.random(-1, 1)*3
     imagem.style.transform = `rotate(${rotacao}deg) scale(1.3)`;
 });
 
